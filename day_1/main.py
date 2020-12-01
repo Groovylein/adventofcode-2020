9eca3c8f4b6242a57d8dc4e8d6f7f1a214b81e00
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2020-12-01 15:59:55 +0100

    Day_1: Part two completed

--- day_1/main.py
+++ day_1/main.py
@@ -1,21 +1,32 @@
 def find_two_numbers(number_list: list) -> tuple:
     search_sum = 2020
     for number in number_list:
-        number_list.remove(number)
+        # number_list.remove(number)
         remaining = search_sum - number
         if remaining in number_list:
             return (number, remaining)
 
 def find_three_numbers(number_list: list) -> tuple:
+    search_sum = 2020
+    number_list.sort()
+    for number in number_list:
+        # number_list.remove(number)
+        if len(number_list) > 0:
+            next_number = number_list[0]
+            intermediate_number = number + next_number
+            if intermediate_number <= search_sum:
+                remaining = search_sum - intermediate_number
+                if remaining in number_list:
+                    return (number,next_number,remaining)
     return (0,0,0)
 
 if __name__ == "__main__":
     with open("input.txt", "r") as openfile:
         numbers_in_file = [int(n) for n in openfile.read().splitlines()]
     # print(numbers_in_file)
-    number_a1, number_b1 = find_two_numbers(numbers_in_file)
-    # print(number_a, number_b)
+    number_a1, number_b1 = find_two_numbers(numbers_in_file[:])
+    # print(number_a1, number_b1)
     print("ANSWER PART ONE:",number_a1 * number_b1)
-
-    number_a2, number_b2, number_c2 = find_three_numbers(numbers_in_file)
-    print(number_a2, number_b2, number_c2)
+    number_a2, number_b2, number_c2 = find_three_numbers(numbers_in_file[:])
+    # print(number_a2, number_b2, number_c2)
+    print("ANSWER PART TWO:",number_a2 * number_b2 * number_c2)
