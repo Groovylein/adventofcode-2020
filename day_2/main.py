9d3ac1d963b91b51184955569eda11edf30bd86e
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2020-12-02 11:20:27 +0100

    Tests done, but answer too low

--- day_2/main.py
+++ day_2/main.py
@@ -1,9 +1,26 @@
-
+import os
 
 def password_validation(condition, password):
-    
-    pass
+    condition_ocurrence, condition_value = condition.split()
+    start_occurence, stop_occurence = condition_ocurrence.split("-")
+    if int(start_occurence) <= int(password.count(condition_value)) <= int(stop_occurence):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
-    pass
+    input_condition_list = []
+    input_password_list = []
+    counter_invalid = 0
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    input_file = os.path.join(current_dir, 'input.txt')
+    with open(input_file, "r") as openfile:
+        for line in openfile:
+            line = line.strip()
+            # print(line)
+            input_condition, input_password = line.split(":")
+            if password_validation(input_condition, input_password) is False:
+                counter_invalid += 1
+    
+    print("ANSWER PART ONE:", counter_invalid)
