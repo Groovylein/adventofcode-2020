350117b8880b168f7dbc7eff55c623f8b8bd07e4
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2020-12-02 10:33:26 +0100

    setup test for day_2

---
+++ day_2/main.py
@@ -0,0 +1,9 @@
+
+
+def password_validation(condition, password):
+    
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ day_2/test_main.py
@@ -0,0 +1,36 @@
+import os
+from day_2 import main
+
+test_input_condition_list = []
+test_input_password_list = []
+current_dir = os.path.dirname(os.path.abspath(__file__))
+input_file = os.path.join(current_dir, 'test_input.txt')
+with open(input_file, "r") as openfile:
+    for line in openfile:
+        line = line.strip()
+        test_input_condition, test_input_password = line.split(":")
+        test_input_condition_list.append(test_input_condition)
+        test_input_password_list.append(test_input_password)
+
+
+def test_valid_1():
+    test_input_condition_1 = test_input_condition_list[0]
+    test_input_password_1 = test_input_password_list[0]
+
+    valid_password = main.password_validation(test_input_condition_1, test_input_password_1)
+    assert valid_password is True
+
+def test_invalid():
+    test_input_condition_2 = test_input_condition_list[1]
+    test_input_password_2 = test_input_password_list[1]
+
+    valid_password = main.password_validation(test_input_condition_2, test_input_password_2)
+    assert valid_password is False
+
+def test_valid_2():
+    test_input_condition_3 = test_input_condition_list[2]
+    test_input_password_3 = test_input_password_list[2]
+
+    valid_password = main.password_validation(test_input_condition_3, test_input_password_3)
+    assert valid_password is True
+
