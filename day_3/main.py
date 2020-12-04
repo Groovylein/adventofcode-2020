2b1b49a4f47e02d4493af6a4bdf1cfe899497dd9
Kiriakos Antoniadis <Groovylein@users.noreply.github.com>
2020-12-04 20:22:52 +0100

    Part one of day 3

--- day_3/main.py
+++ day_3/main.py
@@ -1,8 +1,41 @@
+import os
+
+
 def read_input(file_path):
-    return_dict = {}
-    
-    return return_dict
+    return_list = []
+    with open(file_path, "r") as openfile:
+        for line in openfile:
+            return_list.append(list(line.strip()))
+    return return_list
+
+
+def toboggan_run(geo_list):
+    # right 3, down 1
+    index_row = 0
+    index_column = 0
+    tree_counter = 0
+    for row in geo_list:
+        # print("TREE:", tree_counter)
+        index_column += 3
+        index_row += 1
+        try:
+            if 0 <= index_column < len(geo_list[index_row]):
+                # print(geo_list[index_row][index_column])
+                if geo_list[index_row][index_column] == "#":
+                    tree_counter += 1
+            else:
+                index_column -= len(geo_list[index_row])
+                # print(geo_list[index_row][index_column])
+                if geo_list[index_row][index_column] == "#":
+                    tree_counter += 1
+        except:
+            pass
+    return tree_counter
 
 
 if __name__ == "__main__":
-    pass
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    input_file = os.path.join(current_dir, 'input.txt')
+    input_as_list = read_input(input_file)
+
+    print(toboggan_run(input_as_list))
--- day_3/test_main_part_one.py
+++ day_3/test_main_part_one.py
@@ -6,16 +6,22 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 test_input_file = os.path.join(current_dir, 'test_input.txt')
 test_input_file_simple = os.path.join(current_dir, 'test_input_simple.txt')
 
+
 def test_input():
-    expected_dict = {
-        (0,0) : ".",
-        (0,1) : ".",
-        (1,0) : "#",
-        (1,1) : ".",
-        (2,0) : ".",
-        (2,1) : "#",
-        (3,0) : ".",
-        (3,1) : ".",
-    }
-    input_dict = main.read_input(test_input_file_simple)
-    assert input_dict == expected_dict
+    expected_list = [
+        [".", "."],
+        ["#", "."],
+        [".", "#"],
+        [".", "."],
+    ]
+    input_list = main.read_input(test_input_file_simple)
+    assert input_list == expected_list
+
+
+def test_run():
+
+    input_list = main.read_input(test_input_file)
+
+    trees = main.toboggan_run(input_list)
+
+    assert trees == 7
