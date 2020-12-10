1768df392ffd15905faebeaf4aaa056bb9edb4c3
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2020-12-10 09:24:53 +0100

    Day 9 part Two completed

--- day_9/main.py
+++ day_9/main.py
@@ -24,11 +24,44 @@ def search_invalid_xmas(preamble_list, number_list):
             return search_num
 
 
+def break_xmas(input_list, search_number):
+    outer_loop = True
+    inner_loop = True
+
+    while outer_loop:
+        sum_list = []
+        idx = 0
+        sum_list.append(input_list[idx])
+        idx = (idx + 1) % len(input_list)
+        sum_list.append(input_list[idx])
+
+        while inner_loop:
+            sum_of_list = sum(sum_list)
+            if sum_of_list == search_number:
+                return tuple(sum_list)
+            elif sum_of_list > search_number:
+                inner_loop = False
+            else:
+                idx = (idx + 1) % len(input_list)
+                sum_list.append(input_list[idx])
+
+        input_list.remove(input_list[0])
+        inner_loop = True
+
+
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(current_dir, 'input.txt')
 
-    preable_list, number_list = convert_input(input_file, 25)
-    invalid_number = search_invalid_xmas(preable_list, number_list)
+    p_list, n_list = convert_input(input_file, 25)
+    invalid_number = search_invalid_xmas(p_list.copy(), n_list.copy())
 
     print("PART ONE:", invalid_number)
+
+    input_list = p_list + n_list
+    broken_tuple = break_xmas(input_list, invalid_number)
+
+    max_n = max(broken_tuple)
+    min_n = min(broken_tuple)
+
+    print("PART TWO", max_n + min_n)
--- day_9/test_main.py
+++ day_9/test_main.py
@@ -10,3 +10,9 @@ def test_invalid_xmas():
     test_preable_list, test_number_list = main.convert_input(test_input_file, 5)
     invalid_number = main.search_invalid_xmas(test_preable_list, test_number_list)
     assert invalid_number == 127
+
+
+def test_break_xmas():
+    test_preable_list, test_number_list = main.convert_input(test_input_file, 5)
+    input_list = test_preable_list + test_number_list
+    assert main.break_xmas(input_list, 127) == (15, 25, 47, 40)
