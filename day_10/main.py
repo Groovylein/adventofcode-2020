a79e21ffd72a70e18fad89fecf4ac79cac2c8440
Kiriakos Antoniadis <Groovylein@users.noreply.github.com>
2020-12-14 20:14:01 +0100

    day 10 part two not working :(

--- day_10/main.py
+++ day_10/main.py
@@ -33,12 +33,48 @@ def get_adapter_jolts(numbers_list):
             return one_jolt_diff, three_jolt_diff
 
 
+def create_arrangements(numbers_list):
+    numbers_list.sort()
+
+    max_number = max(numbers_list)
+    max_number = max_number + 3
+
+    arrangements = [numbers_list]
+
+    for i in numbers_list:
+        test_list = numbers_list.copy()
+        test_list.remove(i)
+        new_list = []
+        diff_jolt = 0
+        current_jolt = 1
+        while True:
+            if current_jolt in test_list:
+                test_list.remove(current_jolt)
+                if (current_jolt - diff_jolt) == 1 or \
+                        (current_jolt - diff_jolt) == 3:
+                    new_list.append(current_jolt)
+                    # print(current_jolt)
+                    diff_jolt = current_jolt
+                    current_jolt += 1
+                else:
+                    break
+            else:
+                current_jolt += 1
+
+            if current_jolt == max_number:
+                arrangements.append(new_list)
+                break
+
+    print(arrangements)
+    return arrangements
+
+
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(current_dir, 'input.txt')
     with open(input_file, "r") as openfile:
         numbers_in_file = [int(n) for n in openfile.read().splitlines()]
 
-    one_jolt, three_jolts = get_adapter_jolts(numbers_in_file)
+    one_jolt, three_jolts = get_adapter_jolts(numbers_in_file.copy())
 
     print("FIRST PART:", (one_jolt * three_jolts))
--- day_10/test_main.py
+++ day_10/test_main.py
@@ -4,22 +4,27 @@ from day_10 import main
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 test_input_file_1 = os.path.join(current_dir, 'test_input_1.txt')
+with open(test_input_file_1, "r") as openfile:
+    numbers_in_file_1 = [int(n) for n in openfile.read().splitlines()]
 test_input_file_2 = os.path.join(current_dir, 'test_input_2.txt')
+with open(test_input_file_2, "r") as openfile:
+    numbers_in_file_2 = [int(n) for n in openfile.read().splitlines()]
 
 
 def test_get_jolts_1():
-
-    with open(test_input_file_1, "r") as openfile:
-        numbers_in_file = [int(n) for n in openfile.read().splitlines()]
-    one_jolt, three_jolts = main.get_adapter_jolts(numbers_in_file)
+    one_jolt, three_jolts = main.get_adapter_jolts(numbers_in_file_1)
     assert one_jolt == 7
     assert three_jolts == 5
 
 
+def test_arrangements_1():
+    arrangement = main.create_arrangements(numbers_in_file_1)
+
+    assert len(arrangement) == 8
+
+
 def test_get_jolts_2():
 
-    with open(test_input_file_2, "r") as openfile:
-        numbers_in_file = [int(n) for n in openfile.read().splitlines()]
-    one_jolt, three_jolts = main.get_adapter_jolts(numbers_in_file)
+    one_jolt, three_jolts = main.get_adapter_jolts(numbers_in_file_2)
     assert one_jolt == 22
     assert three_jolts == 10
